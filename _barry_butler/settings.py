f8c53beafc055596718f9dd785df1fe2eb1e40fc
Simon Thompson <simon@sitechindustries.com>
2023-03-10 20:59:06 +0000

    Messages framework for Bootstrap.

--- _barry_butler/settings.py
+++ _barry_butler/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+
 import environ
+from django.contrib.messages import constants as messages
 
 ENV = environ.Env()
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -126,3 +128,12 @@ STATICFILES_FINDERS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+ }
