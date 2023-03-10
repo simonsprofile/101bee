bcc323350c60a30c5376bcf683121135ca40d202
Simon Thompson <simon@sitechindustries.com>
2023-03-10 21:00:54 +0000

    Models to support Hue Bridge connections

--- lights/models.py
+++ lights/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class LightsSettings(models.Model):
+    bridge_ip = models.CharField(
+        'Hue Bridge IP',
+        max_length=128,
+        blank=True,
+        null=True
+    )
+    bridge_user = models.CharField(
+        'Hue Bridge User',
+        max_length=128,
+        blank=True,
+        null=True
+    )
+    bridge_key = models.CharField(
+        'Hue Bridge Key',
+        max_length=128,
+        blank=True,
+        null=True
+    )
