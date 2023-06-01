35453928edb0181028356b6edf576e4826b38bfc
supersim65 <simon@sitechindustries.com>
2023-06-01 10:29:24 +0100

    Frontend updated to Material Design 3 (-esque) style

--- entry/models.py
+++ entry/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Door(models.Model):
+    name = models.CharField(
+        'Door Name',
+        max_length=64,
+        blank=False,
+        null=False
+    )
+    ip = models.CharField(
+        'IP Address',
+        max_length=16,
+        blank=False,
+        null=False
+    )
+    port = models.IntegerField(
+        'Port',
+        null=False
+    )
+    status = models.CharField(
+        'Last Known Status',
+        max_length=64,
+        blank=False,
+        null=False
+    )
+
+class Key(models.Model):
+    door = models.ForeignKey(
+        Door,
+        name="Door",
+        blank=False,
+        null=False
+    )
+    user = models.ForeignKey(
+        User,
+        name="User",
+        blank=False,
+        null=False
+    )
+    recycle = models.BooleanField(
+        'Recycle After Use',
+        default=True,
+        blank=False,
+        null=False
+    )
