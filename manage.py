8050ac7f300a029c78095bcf93f35faf85b70631
simonsprofile <supersim65@gmail.copm>
2023-12-19 17:38:21 +0000

    Rename root directory and global directory to 101bee

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_barry_butler.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_101bee.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
