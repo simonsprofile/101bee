b2646941066b7d67a0e364de4ecb9cbd3ee4a68a
simonsprofile <supersim65@gmail.copm>
2024-09-13 12:50:03 +0100

    Error logging for automated workflows.

--- scribe/admin.py
+++ scribe/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import WorkflowError
 
-# Register your models here.
+admin.site.register(WorkflowError)
--- scribe/models.py
+++ scribe/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class WorkflowError(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    error = models.CharField('Error', max_length=50)
+    description = models.TextField('Description')
