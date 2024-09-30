81e924da9b4f5d89ecb5ede696fdb2b53be4433d
simonsprofile <supersim65@gmail.copm>
2024-09-30 17:17:27 +0100

    Test printing some data from Daikin before making an actual feature.

--- scribe/views.py
+++ scribe/views.py
@@ -9,7 +9,7 @@ class RecordData(View):
     def get(self, request, *args, **kwargs):
         heating = Heating()
         temp_print = heating.record_current_data()
-        r = HttpResponse()
-        r.status_code = 200
-        r.content = json.dump(temp_print)
-        return r
+        return HttpResponse(
+            status_code=200,
+            content=json.dumps(temp_print)
+        )
