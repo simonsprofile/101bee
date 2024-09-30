15bf3097e8e256854cb1a69a894030e4a87c4428
simonsprofile <supersim65@gmail.copm>
2024-09-30 17:14:56 +0100

    Test printing some data from Daikin before making an actual feature.

--- scribe/views.py
+++ scribe/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import HttpResponse
 from django.views.generic import View
 from heating.workflows import Heating
@@ -6,7 +8,8 @@ from heating.workflows import Heating
 class RecordData(View):
     def get(self, request, *args, **kwargs):
         heating = Heating()
-        heating.record_current_data()
+        temp_print = heating.record_current_data()
         r = HttpResponse()
         r.status_code = 200
+        r.content = json.dump(temp_print)
         return r
